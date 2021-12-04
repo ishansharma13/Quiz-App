939d6c460bcbc2fc645b0aa829131be58063f34a
Ishan Sharma <ishansharma1320@gmail.com>
2021-12-04 12:36:39 +0530

    Till Lecture 27

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  // runApp calls the appClass which runs the code
-  runApp(MyApp());
-}
+// void main() {
+//   // runApp calls the appClass which runs the code
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
 
-// MyApp or any app level placeholder class should be inherited from StatelessWidget 
+// MyApp or any app level placeholder class should be inherited from StatelessWidget
 // which requires a build method for its successful implementation
 // MaterialApp does the heavy lifting and shows data
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text("Hello!!! "),
